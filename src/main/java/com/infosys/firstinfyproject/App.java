cb57f643b81a5f862643c7a1c541d61ca00ea548
SujayAgrawal <30735216+SujayAgrawal@users.noreply.github.com>
2020-04-08 08:53:37 +0530

    Spring boot compatible

--- src/main/java/com/infosys/firstinfyproject/App.java
+++ src/main/java/com/infosys/firstinfyproject/App.java
@@ -1,16 +1,21 @@
 package com.infosys.firstinfyproject;
 
 import org.apache.commons.codec.binary.Base64;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App {
 	public static void main(String[] args) {
 		byte[] encodeBase64 = Base64.encodeBase64("Sujay".getBytes());
 		System.out.println("Encoded String: " + new String(encodeBase64));
 		byte[] decodeBase64 = Base64.decodeBase64(encodeBase64);
 		System.out.println("Decoded String: " + new String(decodeBase64));
+		// start app as boot application...
+		SpringApplication.run(App.class, args);
 	}
 }
